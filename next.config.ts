2cbfa9326e82c50c5c0356190576b7cbb79537f3
Faizan_Vhora <faizanvhora999@gmail.com>
2025-09-01 10:57:06 +0530

    vercel changes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  // Only add basePath and assetPrefix in production for GitHub Pages
-  ...(isProd && { 
-    basePath: '/grand-exotica-hospitality',
-    assetPrefix: '/grand-exotica-hospitality/'
-  }),
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
